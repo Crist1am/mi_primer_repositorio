774954c7e092a528fbc866b90d7fcf484df44987
Cristian Vega C <cristianvegacastro@gmail.com>
2023-10-06 19:03:46 -0300

    arreglo de mi segunda version

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 a= input("Si quieres un saludo escribe S, si quieres una emocion escribe E:  ")
 if a == "S":
     print( "Hola mundo")
-if a == E:
+if a == "E":
     print("esto mola mucho") 
 else:
     print("letra no identificada")
